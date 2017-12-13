d40bfd112979831db12961cd112ac460a25c8708
Christopher Walsh <walshcj@uci.edu>
2017-12-13 09:53:45 -0800

    Added featured video metabox to posts, which will replace featured image when video URL is set. Applied this to frontend category lists, and individual post templates.

--- templates/post/content-column-full.php
+++ templates/post/content-column-full.php
@@ -2,9 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                <?php if(has_post_thumbnail()): ?>
+	            <?php if(has_post_thumbnail() && !uciseventeen_has_featured_video()): ?>
 				<?php the_post_thumbnail( 'full', [ 'class' => 'img-responsive' ] ); ?>
                 <div class="caption"><?php the_post_thumbnail_caption(); ?></div>
+	            <?php elseif(has_post_thumbnail() && uciseventeen_has_featured_video()): ?>
+		            <?php echo wp_oembed_get(uciseventeen_get_featured_video_url()); ?>
                 <?php endif; ?>
                 <h1 class="page-heading"><?php the_title(); ?></h1>
 	            <?php the_date(); ?>
