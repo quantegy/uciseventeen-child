4f5f938a4ef06a955ffec36eb2b5e70cd21f26dc
Christopher Walsh <walshcj@uci.edu>
2018-02-22 17:03:42 -0800

    Moved theme stylesheet to the end of the styles cascade. Added a WP filter hook to handle default WP widget titles. Reorganized template HTML to correspond with new format. Applied new styles.

--- templates/post/content-row-wrap.php
+++ templates/post/content-row-wrap.php
@@ -4,7 +4,7 @@
             <div class="col-md-12 clearfix">
                 <h1 class="page-heading"><?php the_title(); ?></h1>
 
-                <div><?php the_excerpt(); ?></div>
+                <h2 class="page-subheadline"><?php the_excerpt(); ?></h2>
 
 	            <?php if(has_post_thumbnail() && !uciseventeen_has_featured_video()): ?>
                 <div class="portrait-header">
@@ -15,16 +15,18 @@
 		            <?php echo wp_oembed_get(uciseventeen_get_featured_video_url()); ?>
                 <?php endif; ?>
 
-	            <?php echo apply_filters('cj_authorship_authors', cj_authorship_get_author_names(get_the_ID()), array(
-		            'container_start' => '',
-		            'container_end' => '',
-		            'before_author' => '',
-		            'after_author' => '',
-		            'prefix' => 'by',
-		            'separator' => ' '
-	            )); ?>
-                |
-	            <?php the_date(); ?>
+	            <div class="page-date">
+                    <?php echo apply_filters('cj_authorship_authors', cj_authorship_get_author_names(get_the_ID()), array(
+                        'container_start' => '',
+                        'container_end' => ' | ',
+                        'before_author' => '',
+                        'after_author' => '',
+                        'prefix' => 'by',
+                        'separator' => ' '
+                    )); ?>
+
+                    <?php the_date(); ?>
+                </div>
 
                 <p><?php the_content(); ?></p>
             </div>
